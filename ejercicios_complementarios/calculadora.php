cd9bed0c0c2a9d4e01546d5c707acc962e613d12
luciagaarciaaa <lucia55ee@gmail.com>
2024-11-11 10:57:50 +0100

    actualizado

---
+++ ejercicios_complementarios/calculadora.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @autor lucia
+ * Crea un formulario que actúe como una calculadora básica (suma, resta,
+ *multiplicación y división). El usuario debe ingresar dos números y seleccionar la
+ *operación que desea realizar. Cuando el formulario se envíe, muestra el
+ *resultado en la misma página.
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora</title>
+</head>
+<body>
+    <form method="post" action="">
+        <input type="number">
+
+
+    </form>
+</body>
+</html>
